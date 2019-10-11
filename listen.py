dd2d7a1f7d810bda122cb23d79ae53a13d9615b8
Abhishek <abhisheks@iitbhilai.ac.in>
2019-10-11 22:53:19 +0530

    Saves chats to file

--- listen.py
+++ listen.py
@@ -1,4 +1,8 @@
-#This Listens for new messages on a port and displays it on terminal.
+#This Listens for new messages on a port.
+#The data recivied is JSON Structured
+# {"ip":,"content":}  
+#ip-> ip address of node sending the message.
+#content-> content of message 
 
 import socket
 import sys
@@ -13,5 +17,10 @@ while True:
     server_socket.listen(1)
     conn, address = server_socket.accept()  # accept new connection
     data = conn.recv(1024).decode()
-    print(str(data))
+    data = json.loads(data)
+    chatline=data["content"]+"\n"
+    print(data["content"])
+    file = open("chats.txt",'a') 
+    file.write(chatline) 
+    file.close() 
     conn.close()  # close the connection
