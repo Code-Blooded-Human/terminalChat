0ce0971875a07fec63992c6d2186aec698607854
Abhishek <abhisheks@iitbhilai.ac.in>
2019-10-11 23:17:31 +0530

    saves chats to indiviual file

--- listen.py
+++ listen.py
@@ -1,9 +1,3 @@
-#This Listens for new messages on a port.
-#The data recivied is JSON Structured
-# {"ip":,"content":}  
-#ip-> ip address of node sending the message.
-#content-> content of message 
-#added directional arrow
 import socket
 import sys
 import json
@@ -12,15 +6,16 @@ import os
 port = int(sys.argv[1])  # initiate port no above 1024
 server_socket = socket.socket() 
 server_socket.bind(('', port))
-
+os.system("rm -rf chats")
+os.system("mkdir chats")
 while True:
     server_socket.listen(1)
     conn, address = server_socket.accept()  # accept new connection
     data = conn.recv(1024).decode()
     data = json.loads(data)
-    chatline="-->>"+data["content"]+"\n"
+    chatline=" -->> "+data["content"]+"\n"
     print(data["content"])
-    file = open("chats.txt",'a') 
+    file = open("chats/"+str(data["ip"]),'a') 
     file.write(chatline) 
     file.close() 
     conn.close()  # close the connection
