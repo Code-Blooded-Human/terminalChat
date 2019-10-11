b6727b77f082ced7b6d255633706da20989700c6
Kumari Rani <krani@iitbhilai.ac.in>
2019-10-11 22:48:19 +0530

    adds arrows to file

--- send.py
+++ send.py
@@ -2,7 +2,7 @@
 
 import socket
 import sys
-
+import json
 
 host = sys.argv[1]  # ip of reciver
 port = int(sys.argv[2])  # port 
@@ -11,6 +11,9 @@ client_socket = socket.socket()  # instantiate
 client_socket.connect((host, port))  # connect to the server
 
 message = raw_input("")  # take input
+file = open("chats/"+host ,"a")
+file.write("<<--"+message+"\n")
+file.close()
 
 client_socket.send(message.encode())  # send message
 
